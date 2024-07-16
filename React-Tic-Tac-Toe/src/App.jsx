0115ab4c135a435265d44367073b0f74494d847b
popo <popo@popo-vivo.gmail.com>
2024-07-16 11:58:36 +0700

    Tic Tac Toe

--- React-Tic-Tac-Toe/src/App.jsx
+++ React-Tic-Tac-Toe/src/App.jsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
-import Button from './components/Button'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Button from './components/Button';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+export default function TicTacToe() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
-  function resetClick() {
-    setSquares(Array(9).fill(null));
-    setXIsNext(true);
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
   }
 
+  function handlePlay(nextPoint) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextPoint];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function resetGame() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move} className='moves'>
+        <span onClick={() => jumpTo(move)}>{description}</span>
+      </li>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <div className="game">
+        <div className="gameboard">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    if(squares[i] || callculateWinner(squares)) return;
+    if (squares[i] || calculateWinner(squares)) return;
     const nextPoint = squares.slice();
     nextPoint[i] = xIsNext ? 'X' : 'O';
-    setSquares(nextPoint);
-    setXIsNext(!xIsNext);
-
+    onPlay(nextPoint);
   }
 
   let status = '';
-  let winner = callculateWinner(squares)
-  if(winner) {
-    status = "Winner : " + winner;
+  let winner = calculateWinner(squares);
+  if (winner) {
+    status = "Winner: " + winner;
   } else {
-    status = "Next Player : " + (xIsNext ? 'X' : 'O');
+    status = "Next Player: " + (xIsNext ? 'X' : 'O');
   }
 
   return (
@@ -35,45 +75,41 @@ export default function Board() {
       <div className="container">
         <h1>React Tic Tac Toe</h1>
         <div className="board">
-            <Button value={squares[0]} onPointClick={() => handleClick(0)} />
-            <Button value={squares[1]} onPointClick={() => handleClick(1)} />
-            <Button value={squares[2]} onPointClick={() => handleClick(2)} />
-            <Button value={squares[3]} onPointClick={() => handleClick(3)} />
-            <Button value={squares[4]} onPointClick={() => handleClick(4)} />
-            <Button value={squares[5]} onPointClick={() => handleClick(5)} />
-            <Button value={squares[6]} onPointClick={() => handleClick(6)} />
-            <Button value={squares[7]} onPointClick={() => handleClick(7)} />
-            <Button value={squares[8]} onPointClick={() => handleClick(8)} />
+          <Button value={squares[0]} onPointClick={() => handleClick(0)} />
+          <Button value={squares[1]} onPointClick={() => handleClick(1)} />
+          <Button value={squares[2]} onPointClick={() => handleClick(2)} />
+          <Button value={squares[3]} onPointClick={() => handleClick(3)} />
+          <Button value={squares[4]} onPointClick={() => handleClick(4)} />
+          <Button value={squares[5]} onPointClick={() => handleClick(5)} />
+          <Button value={squares[6]} onPointClick={() => handleClick(6)} />
+          <Button value={squares[7]} onPointClick={() => handleClick(7)} />
+          <Button value={squares[8]} onPointClick={() => handleClick(8)} />
         </div>
-      <span className="status">{status}</span>
-      {winner ? <span className='resetButton' onClick={resetClick}>Reset / Play again</span> : ''}
+        <span className="status">{status}</span>
+        {winner ? <span className='resetButton'>Reset / Play again</span> : ''}
       </div>
     </>
   );
 }
 
-function callculateWinner(squares) {
+function calculateWinner(squares) {
   const lines = [
-    // vertical
     [0, 1, 2],
+    [3, 4, 5],
     [6, 7, 8],
-    [6, 7, 8],
-    // horizontal
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    // diagonal
     [0, 4, 8],
     [2, 4, 6],
   ];
 
-  for(let i = 0; i < lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
 
-  return false;
+  return null;
 }
